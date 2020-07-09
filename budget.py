c276fb212e99c8fb54ebd94a15f064b7942a1ed3
NikolaiB <boikonikolai@mail.ru>
2020-07-10 00:02:34 +0600

    allure reporter was added

--- budget.py
+++ budget.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.chrome.webdriver import WebDriver
 from pages.budget_page import BudgetPage
+import allure
 
 
 class TestBudget:
@@ -8,6 +9,7 @@ class TestBudget:
         self.driver = WebDriver(executable_path="/Users/nikolai/Downloads/chromedriver")
         self.budgetPage = BudgetPage(self.driver)
 
+    @allure.title("Add a new item")
     def test_add_item(self):
         self.budgetPage.open()
         self.budgetPage.select_category('Income')
@@ -17,6 +19,7 @@ class TestBudget:
         assert self.budgetPage.cell_value(7).text == '$1,000.00'
         assert self.budgetPage.working_balance().text == '$3,213.93'
 
+    @allure.title("Update existing item")
     def test_update_item(self):
         self.budgetPage.open()
         self.budgetPage.row(6).click()
@@ -25,6 +28,7 @@ class TestBudget:
         assert self.budgetPage.cell_value(6).text == '$57,000.00'
         assert self.budgetPage.working_balance().text == '$53,513.93'
 
+    @allure.title("Delete an item")
     def test_delete_item(self):
         self.budgetPage.open()
         assert self.budgetPage.working_balance().text == '$2,213.93'
--- pages/budget_page.py
+++ pages/budget_page.py
@@ -1,6 +1,3 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import allure
 
